0f8b45ef5fe8217f5a47d22a6d5e7fc91c9104db
ghager93 <ghager93@gmail.com>
2023-03-29 10:59:45 +1100

    Only draws on canvas now.

--- core/states/canvas.py
+++ core/states/canvas.py
@@ -26,6 +26,10 @@ class CanvasState(State):
         self._lines = []
         self._current_line = []
 
+        self._font = pg.font.Font(size=32)
+
+        self._is_mouse_on_canvas = False
+
 
     def handle_event(self, event: pg.event.Event) -> None:
         pass
@@ -41,7 +45,12 @@ class CanvasState(State):
         for i in range(len(self._current_line)-1):
             pg.draw.line(screen, self._brush_colour, self._current_line[i], self._current_line[i+1], self._brush_width)
 
+        screen.blit(self._font.render(str(self._is_mouse_on_canvas), True, "black"), (50, 0))
+
     def update(self, dt: int) -> None:
+        mouse_pos = pg.mouse.get_pos()
+        self._is_mouse_on_canvas = self._canvas_rect.collidepoint(mouse_pos)
+
         # Start of line
         if self._is_line_start():
             self._is_drawing = True
@@ -49,20 +58,21 @@ class CanvasState(State):
         # End of line
         if self._is_line_end():
             # If on border, add current mouse position as last point on line
-            if utils.is_border_pos(pg.mouse.get_pos()):
-                self._current_line.append(pg.mouse.get_pos())
+            if utils.is_border_pos(mouse_pos):
+                self._current_line.append(mouse_pos)
             self._is_drawing = False
             self._lines.append(self._current_line)
             self._current_line = list()
 
         if self._is_drawing:
-            self._current_line.append(pg.mouse.get_pos())
+            self._current_line.append(mouse_pos)
+
 
     def _is_line_start(self):
-        return pg.mouse.get_pressed()[0] and not self._is_drawing
+        return self._is_mouse_on_canvas and pg.mouse.get_pressed()[0] and not self._is_drawing
     
     def _is_line_end(self):
-        return (not pg.mouse.get_pressed()[0] or utils.is_border_pos(pg.mouse.get_pos())) and self._is_drawing
+        return (not pg.mouse.get_pressed()[0] or not self._is_mouse_on_canvas) and self._is_drawing
 
     def teardown(self) -> None:
         pass
